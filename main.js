fc9fe6dee65be9cb40a92985ab646e599e130954
FiveWattFender <37257319+FiveWattFender@users.noreply.github.com>
2021-03-07 00:19:38 -0500

    prep for main.js rewrite

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
-require('dotenv').config();
 const client = new Discord.Client();
+const config = require('./config.json')
+require('dotenv').config();
 
 client.on('ready', () => {
     console.log('Bot Running');
@@ -9,13 +10,4 @@ client.on('ready', () => {
 // logon to server with token
 client.login(process.env.BOT_TOKEN) 
 
-// Basic commands, may be moved later (will also be converted to switches, im just lazy)
-client.on('message', (msg) => {
-    if (msg.channel.type !='text' || msg.author.bot || !msg.startsWith('!'))
-    return;
-    
-    // ping pong lol
-    if (msg.content === '*ping') {
-        msg.channel.send('pong');
-    }
-});
+
